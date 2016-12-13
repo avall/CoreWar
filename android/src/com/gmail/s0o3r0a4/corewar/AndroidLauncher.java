3676a944c425959ce8761b881bb02ecb44fd1a76
s0ra <s0o3r0a4@gmail.com>
2016-12-13 15:31:33 -0800

    Initial commit

---
+++ android/src/com/gmail/s0o3r0a4/corewar/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.gmail.s0o3r0a4.corewar;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.gmail.s0o3r0a4.corewar.GameActivity;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameActivity(), config);
+	}
+}
---
+++ desktop/src/com/gmail/s0o3r0a4/corewar/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gmail.s0o3r0a4.corewar.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gmail.s0o3r0a4.corewar.GameActivity;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameActivity(), config);
+	}
+}
