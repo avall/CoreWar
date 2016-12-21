6380306b5e7c1410229080981ef8631f5d53ed43
s0ra <s0o3r0a4@gmail.com>
2016-12-20 17:47:12 -0800

    Add assets, skin and its manager

---
+++ core/src/com/gmail/s0o3r0a4/corewar/assets/Assets.java
@@ -0,0 +1,28 @@
+package com.gmail.s0o3r0a4.corewar.assets;
+
+import com.badlogic.gdx.assets.AssetDescriptor;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
+public class Assets {
+    public AssetManager manager = new AssetManager();
+
+    public static final AssetDescriptor<Texture> blockTexture =
+            new AssetDescriptor<Texture>("block.png", Texture.class);
+
+    public static final AssetDescriptor<Skin> uiSkin =
+            new AssetDescriptor<Skin>("default-ui/uiskin.json", Skin.class);
+
+    public void load()
+    {
+        manager.load(blockTexture);
+        manager.load(uiSkin);
+    }
+
+    public void dispose()
+    {
+        manager.dispose();
+    }
+}
+
