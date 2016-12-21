ad1f615f8eef551da332d8fdace9532fd0c64cc3
s0ra <s0o3r0a4@gmail.com>
2016-12-20 17:53:14 -0800

    Add menu

---
+++ core/src/com/gmail/s0o3r0a4/corewar/screen/MainMenuScreen.java
@@ -0,0 +1,54 @@
+package com.gmail.s0o3r0a4.corewar.screen;
+
+import com.badlogic.gdx.Screen;
+import com.gmail.s0o3r0a4.corewar.assets.Assets;
+
+public class MainMenuScreen implements Screen
+{
+    public MainMenuScreen(Assets assets)
+    {
+
+    }
+
+    @Override
+    public void show()
+    {
+
+    }
+
+    @Override
+    public void render(float delta)
+    {
+
+    }
+
+    @Override
+    public void resize(int width, int height)
+    {
+
+    }
+
+    @Override
+    public void pause()
+    {
+
+    }
+
+    @Override
+    public void resume()
+    {
+
+    }
+
+    @Override
+    public void hide()
+    {
+
+    }
+
+    @Override
+    public void dispose()
+    {
+
+    }
+}
