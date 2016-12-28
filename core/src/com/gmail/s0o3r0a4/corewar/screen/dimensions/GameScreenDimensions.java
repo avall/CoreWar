50b41f175b36bc1216fd70b6effa29e4ef6553ee
s0ra <s0o3r0a4@gmail.com>
2016-12-27 22:39:10 -0800

    Dimension constants

---
+++ core/src/com/gmail/s0o3r0a4/corewar/screen/dimensions/GameScreenDimensions.java
@@ -0,0 +1,14 @@
+package com.gmail.s0o3r0a4.corewar.screen.dimensions;
+
+public class GameScreenDimensions
+{
+    public static final float CORE_OFFSET_X1 = 0f;
+    public static final float CORE_OFFSET_X2 = 0f;
+    public static final float CORE_OFFSET_Y = 0f;
+    public static final float CORE_OFFSET_Y2 = 0f;
+    public static final float CORE_COL_GAP = 1f;
+    public static final float CORE_ROW_GAP = 1f;
+
+    public static final float BLOCKS_COL = 16;
+    public static final float BLOCKS_ROW = 500;
+}
